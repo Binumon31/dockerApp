da63f4e95fe5b14457b9f6748a0a140b59c89dd8
Binumon31 <binumonjacob3197@gmail.com>
2023-07-19 23:30:17 +0530

    consoling logs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+console.log("test")
 app.get('/', (request, response) => {
   response.status(200).json({
     message: 'Hello Docker!',
